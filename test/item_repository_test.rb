e8ec3e81f06ebe4449055c87363bf53b3db34190
Jennifer <jennifersoden@hotmail.com>
2016-03-02 13:35:34 -0700

    Working on repos

---
+++ test/item_repository_test.rb
@@ -0,0 +1,53 @@
+require_relative 'test_helper'
+require 'minitest/autorun'
+require_relative '../lib/item_repository'
+
+class ItemRepositoryTest < Minitest::Test
+  def setup
+    @test_helper = TestHelper.new
+    @items = @test_helper.array_of_items
+    @item_repository = ItemRepository.new(@items)
+  end
+
+  def test_all_returns_array_of_item_instances
+    assert_equal [], @item_repository.all
+  end
+
+  # def test_find_by_id_returns_item_with_matching_id
+  # end
+  #
+  # def test_find_by_id_returns_nil_when_none_match
+  # end
+  #
+  # def test_find_by_name_returns_instance_of_item
+  # end
+  #
+  # def test_find_all_by_name_returns_nil_when_none_match
+  # end
+  #
+  # def test_find_all_with_description_returns_an_empty_array
+  # end
+  #
+  # def test_find_all_with_description_returns_a_populated_array
+  # end
+  #
+  # def test_find_all_by_price_returns_an_empty_array_when_nothing_is_found
+  # end
+  #
+  # def test_find_all_by_price_returns_an_item_when_price_matches
+  # end
+  #
+  # def test_find_all_by_price_in_range_returns_an_empty_array_when_nothing_is_found
+  # end
+  #
+  # def test_find_all_by_price_in_range_return_items_in_an_array_that_match
+  # end
+  #
+  # def test_find_all_by_merchant_id_returns_empty_array_when_none_match
+  # end
+  #
+  # def test_find_all_by_merchant_id_returns_instances_of_item_where_merch_id_matches
+  # end
+  #
+
+end
