145e7922e87f7aa52cd3aad42c373dfe1f1db305
Ashwin Rao <theonlyrao@gmail.com>
2016-03-01 21:12:05 -0700

    sales engine tests passing

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,27 +2,43 @@ require 'pry'
 require 'csv'
 
 class SalesEngine
+
   def from_csv(hash)
-    se_hash = Hash.new
-    item_database = CSV.new hash[:items], headers: true, header_converters: :symbol
-    merchant_database = CSV.new hash[:merchants], headers: true, header_converters: :symbol
-    to_hash(item_database)
-    to_hash(merchant_database)
-    se_hash = {:items => to_hash(item_database), :merchants => to_hash(merchant_database)}
+    items_array = items_array_maker(hash[:items])
+    merchants_array = merchants_array_maker(hash[:merchants])
+    sales_engine_hash = {:items => items_array,
+                         :merchants => merchants_array}
   end
 
-  def to_hash(csv)
-    csv.each do |row|
-      id = row[:id]
-      name = row[:name]
-      created_at = row[:created_at]
-      updated_at = row[:updated_at]
-      # if csv == :items
-      #   description = row[:description]
-      #   unit_price = row[:unit_price]
-      #   merchant_id = row[:merchant_id]
-      # end
+  def items_array_maker(items_filepath)
+    contents = CSV.open items_filepath, headers: true, header_converters: :symbol
+    items = []
+    contents.each do |row|
+      hash = Hash.new
+      hash[:id] = row[:id]
+      hash[:name] = row[:name]
+      hash[:description] = row[:description]
+      hash[:unit_price] = row[:unit_price]
+      hash[:merchant_id] = row[:merchant_id]
+      hash[:created_at] = row[:created_at]
+      hash[:updated_at] = row[:updated_at]
+      items << hash
     end
+    items
+  end
+
+  def merchants_array_maker(merchants_filepath)
+    contents = CSV.open merchants_filepath, headers: true, header_converters: :symbol
+    merchants = []
+    contents.each do |row|
+      hash = Hash.new
+      hash[:id] = row[:id]
+      hash[:name] = row[:name]
+      hash[:created_at] = row[:created_at]
+      hash[:updated_at] = row[:updated_at]
+      merchants << hash
+    end
+    merchants
   end
 
 end
