4c476a299c1cac857d5adc28b3860b07c6156191
Jennifer <jennifersoden@hotmail.com>
2016-03-02 15:55:29 -0700

    iteration 0 tested

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -1,6 +1,7 @@
 require_relative 'test_helper_2'
 require 'minitest/autorun'
-require_relative 'merchant_repository'
+require_relative '../lib/merchant_repository'
+
 
 class MerchantRepositoryTest < Minitest::Test
   def setup
@@ -10,17 +11,33 @@ class MerchantRepositoryTest < Minitest::Test
   end
 
   def test_all_returns_array_of_all_Merchant_instances
-    
+    assert_equal Array, @merchant_repository.all.class
+    assert_equal 3, @merchant_repository.all.count
+    assert_equal "Bill", @merchant_repository.all[0].name
   end
 
-  # def test_find_by_id_returns_nil
-  # end
-  #
-  # def test_find_all_returns_instances_of_Merchant
-  # end
-  #
-  # find_by_name - returns either nil or an instance of Merchant having done a case insensitive search
-  # find_all_by_name - returns either [] or one or more matches which contain the supplied name fragment, case insensitive
+  def test_find_by_id_returns_nil
+    assert_equal nil, @merchant_repository.find_by_id(72)
+  end
 
+  def test_find_by_id_returns_instances_of_Merchant
+    assert_equal "SparklePantsLarry", @merchant_repository.find_by_id(555555555).name
+  end
 
+  def test_find_by_name_returns_nil_when_no_match_is_found
+    assert_equal nil, @merchant_repository.find_by_name("Ashwin")
+  end
+
+  def test_find_by_name_returns_an_instance_of_Merchant
+    assert_equal "Bill", @merchant_repository.find_by_name("Bill").name
+  end
+
+  def test_find_all_by_name_returns_an_empty_array
+    assert_equal [], @merchant_repository.find_all_by_name("nobody")
+  end
+
+  def test_find_all_by_name_returns_one_plus_matches_that_contain_fragment
+    assert_equal 2, @merchant_repository.find_all_by_name("l").count
+  end
+#more testS? CHECK SENSITIVITY
 end
