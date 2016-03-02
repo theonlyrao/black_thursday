71fdb8c36a28da952c4d5e59ae669dfa2ea32c21
Ashwin Rao <theonlyrao@gmail.com>
2016-03-02 12:52:17 -0700

    now sales engine has class methods

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -5,14 +5,14 @@ require_relative 'item'
 
 class SalesEngine
 
-  def from_csv(hash)
+  def self.from_csv(hash)
     items_array = item_instance_maker(hash[:items])
     merchants_array = merchant_instance_maker(hash[:merchants])
     sales_engine_hash = {:items => items_array,
                          :merchants => merchants_array}
   end
 
-  def item_instance_maker(items_filepath)
+  def self.item_instance_maker(items_filepath)
     contents = CSV.open items_filepath, headers: true, header_converters: :symbol
     items = []
     contents.each do |row|
@@ -29,7 +29,7 @@ class SalesEngine
     items
   end
 
-  def merchant_instance_maker(merchants_filepath)
+  def self.merchant_instance_maker(merchants_filepath)
     contents = CSV.open merchants_filepath, headers: true, header_converters: :symbol
     merchants = []
     contents.each do |row|
