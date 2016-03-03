3eac1250b015bf59c09608889dde175e42eea66f
Ashwin Rao <theonlyrao@gmail.com>
2016-03-02 21:35:21 -0700

    first half of relationships layer for iteration 1 working

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,4 +1,7 @@
 require 'pry'
+require_relative 'item_repository'
+require_relative 'items_by_merchant_searcher'
+require_relative 'sales_engine'
 
 class Merchant
 
@@ -7,6 +10,19 @@ class Merchant
   def initialize(hash)
     @id = hash[:id].to_i
     @name = hash[:name]
+  end
+
+  def items
+    SalesEngine.push_to_merchant_searcher(self.id)
+    Merchant.items_being_sold
+  end
+
+  def self.push_items_to_merchant(items)
+    @merchants_items_being_sold = items
+  end
+
+  def self.items_being_sold
+    @merchants_items_being_sold
   end
 
 end
